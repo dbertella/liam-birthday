8733b75a16768bddd4073ea274c0dcf9df91821c
dbertella <daniele.bertella@gmail.com>
2018-12-08 12:04:06 +0000

    Updates

---
+++ precache-manifest.c67f2a6562e945151daba22c878e25e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58ccc1e35782a3999ea64922723e9c17",
+    "url": "/liam-birthday/static/media/liam.58ccc1e3.jpeg"
+  },
+  {
+    "revision": "2cbeb5e1a8e2d99580e8",
+    "url": "/liam-birthday/static/js/runtime~main.2cbeb5e1.js"
+  },
+  {
+    "revision": "c410c257f4802214ad11",
+    "url": "/liam-birthday/static/js/main.c410c257.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/liam-birthday/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "c410c257f4802214ad11",
+    "url": "/liam-birthday/static/css/main.679285fe.chunk.css"
+  },
+  {
+    "revision": "bce490d8ee40a8e6eac910c103aa3be4",
+    "url": "/liam-birthday/index.html"
+  }
+];
